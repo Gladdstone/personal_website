411768323e26179384772459ecdb507db7ff9e16
Joe Farrell <jdf4921@rit.edu>
2017-12-16 15:50:22 -0500

    Recreated with php and bootstrap support

--- index.php
+++ index.php
@@ -1,7 +1,124 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: JosephFarrell
- * Date: 12/15/2017
- * Time: 10:06 PM
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Joseph Farrell</title>
+    <meta name="description" content="">
+    <meta name="author" content="Joseph Farrell">
+    <meta name="keywords" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Favicon -->
+    <link rel="icon" href="assets/tardis_favicon.png"/>
+
+    <!-- Icon library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+</head>
+<body>
+    <nav class="navbar navbar-default navbar-doublerow navbar-trans navbar-fixed-top">
+        <!-- top nav -->
+        <nav class="navbar navbar-top hidden-xs">
+            <div class="container">
+                <!-- left nav top -->
+                <ul class="nav navbar-nav pull-left">
+                </ul>
+                <!-- right nav top: social media -->
+                <ul class="nav navbar-nav pull-right">
+                    <li><a href="https://twitter.com/@Pithpifth" class="fa fa-twitter"></a></li>
+                    <li><a href="https://www.facebook.com/joe.farrell.96930?ref=bookmarks" class="fa fa-facebook"></a></li>
+                </ul>
+            </div>
+            <div class="dividline light-grey"></div>
+        </nav>
+        <!-- down nav -->
+        <nav class="navbar navbar-down">
+            <div class="container">
+                <div class="flex-container">
+                    <!-- left side-->
+                    <div class="navbar-header flex-item">
+                        <div class="navbar-brand" href="index.php">Joseph Farrell</div>
+                    </div>
+                    <!-- middle -->
+                    <ul class="nav navbar-nav flex-item hidden-xs">
+                    </ul>
+                    <!-- right: main navigation -->
+                    <ul class="nav navbar-nav flex-item hidden-xs pull-right">
+                        <li><a href="#">Profile</a></li>
+                        <li><a href="#">Experience</a></li>
+                        <li><a href="#">Abilities</a></li>
+                        <li><a href="#">Projects</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                    <!-- dropdown only moblie -->
+                    <div class="dropdown visible-xs pull-right">
+                        <button class="btn btn-default dropdown-toggle " type="button" id="dropdownmenu" data-toggle="dropdown">
+                            <span class="glyphicon glyphicon-align-justify"></span>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Profile</a></li>
+                            <li><a href="#">Experiences</a></li>
+                            <li><a href="#">Projects</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </nav>
+    <!--bg img  -->
+    <header>
+        <img src="assets/loz.jpg" style="width:100%">
+        <div class="arrow bounce">
+            <span>Scroll Down</span><br/>
+            <a class="fa fa-arrow-down fa-2x" href="#"></a>
+        </div>
+    </header>
+    <div class="light-tan">
+        <br/><br/>
+        <div class="container dark-tan border-rounded">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h3>Profile</h3>
+                    <hr/>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-1"></div>
+                <div class="col-sm-4">
+                    <img class="img-circle pdlft" src="assets/joe.jpg"/>
+                </div>
+                <div class="col-sm-6">
+                    <h4>Details</h4>
+                    <p class="bold">Name: </p>
+                    <p>Joseph Farrell</p>
+                    <p class="bold">Age: </p>
+                    <p>20 years</p>
+                    <p class="bold">Location:</p>
+                    <p>Endicott, NY</p>
+                </div>
+            </div>
+            <br/><br/>
+            <div class="row">
+                <div class="col-sm-4">
+                    <h4>About Me</h4>
+                    <hr/>
+                </div>
+            </div>
+            <div class="row">
+
+            </div>
+        </div>
+        <br/><br/>
+    </div>
+
+<script src="jquery-3.2.1.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/scripts.js"></script>
+</body>
+</html>
