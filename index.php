fd21ca7ecd9a45aabb58717b015cc5f28640ee8b
Joe Farrell <jdf4921@rit.edu>
2017-12-16 23:38:11 -0500

    Fleshed out educational experience content

--- index.php
+++ index.php
@@ -49,7 +49,7 @@
                     <!-- right: main navigation -->
                     <ul class="nav navbar-nav flex-item hidden-xs pull-right">
                         <li><a href="#profile">Profile</a></li>
-                        <li><a href="#">Experience</a></li>
+                        <li><a href="#experiences">Experience</a></li>
                         <li><a href="#">Abilities</a></li>
                         <li><a href="#">Projects</a></li>
                         <li><a href="#">Contact</a></li>
@@ -61,7 +61,7 @@
                         </button>
                         <ul class="dropdown-menu">
                             <li><a href="#profile">Profile</a></li>
-                            <li><a href="#">Experiences</a></li>
+                            <li><a href="#experiences">Experiences</a></li>
                             <li><a href="#">Projects</a></li>
                             <li role="separator" class="divider"></li>
                             <li><a href="#">Contact</a></li>
@@ -90,8 +90,7 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-sm-1"></div>
-                <div class="col-sm-4">
+                <div class="col-md-4 col-md-offset-1">
                     <img class="img-circle pdlft" src="assets/joe.jpg"/>
                 </div>
                 <div class="col-sm-6">
@@ -106,17 +105,83 @@
             </div>
             <br/><br/>
             <div class="row">
-                <div class="col-sm-4">
+                <div class="col-md-4 col-md-offset-1">
                     <h4>About Me</h4>
                     <hr/>
                 </div>
             </div>
             <div class="row">
-
+                <div class="col-md-8 col-md-offset-1">
+                    <p>I am a third-year Computer Science major at Rochester Institute of Technology. My experience is
+                        primarily in object-oriented programming, although I have rudimentary experience writing brief
+                        portions of functional code. I desire to work in an area in which both creative design and program
+                        efficiency are prioritized.  My main focus as a professional developer is on process improvement
+                        and what changes can or must be made in order to save time or money, and improve the employee or
+                        consumer experience.
+                    </p>
+                </div>
             </div>
+            <br/>
         </div>
         <br/><br/>
     </div>
+    <br/><br/>
+    <div>
+        <!-- Experiences -->
+        <div id="experiences" class="container dark-tan border-rounded">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                    <h3 class="centered">Experiences</h3>
+                    <hr/>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <h3>Education</h3>
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-md-4 col-md-offset-1">
+                    <p class="bold">Rochester Institute of Technology</p>
+                    <p>August, 2015 - Present</p>
+                </div>
+                <div class="col-md-6">
+                    <p class="bold">Bachelors of Science - Computer Science</p>
+                    <p>
+                        Coursework and independent study have involved both the fundamentals and intricacies of
+                        programming and software design, including such topics as development models as well as standard
+                        CS topics such as data structures, algorithms, runtime optimization, and memory management. I
+                        am currently studying web development and server maintenance as both a minor and a personal
+                        interest/hobby. I run a small, low-traffic website on a server running Ubuntu 16.04 and the
+                        latest apache2 release which I have personally configured for my purposes.
+                        I have a decade of experience on both violin and flute, including both performance and
+                        composition, solo, in small groups, and large orchestras.
+                        I am the Co-founder and Vice President of RIT eSports organization, a club which at the end of
+                        its first year boasted over 200 active members and currently has an estimated 500 members.
+                        Overseen small and largescale projects, including planning and production of an eSports
+                        intramural league within the campus.
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-md-offset-1">
+                    <p class="bold">Union Endicott High School</p>
+                    <p>September, 2011 - June, 2015</p>
+                </div>
+                <div class="col-md-6">
+                    <p class="bold">High School</p>
+                    <p>
+                        Graduated with honors in 2015. My focus throughout grade school was entirely on music, and
+                        it wasn't until my senior year, while taking AP Computer Science that I began to consider
+                        programming as a potential career.
+                    </p>
+                </div>
+            </div>
+            <br/>
+        </div>
+    </div>
+    <br/><br/>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="js/bootstrap.js"></script>
 <script src="js/scripts.js"></script>
